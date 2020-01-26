d29929d37cd7b9f0bb801924b08c44c84eb2b86b
Vitor <vitoraderaldo10@gmail.com>
2020-01-26 18:11:51 -0300

    Navigator feito

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-/**
- * @format
- */
-
-import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import 'react-native-gesture-handler'
+import {AppRegistry} from 'react-native'
+import {name as appName} from './app.json'
+import Navigator from './src/Navigator'
 console.disableYellowBox = true
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Navigator)
---
+++ src/Navigator.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {createAppContainer} from 'react-navigation'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import {Feed} from './screens/Feed'
+
+const RouteConfigs = {
+    Feed: {
+        name:'Feed',
+        screen: Feed,
+        navigationOptions: {
+            title:'Feed',
+            tabBarIcon: ({tintColor}) =>  
+                <Icon name='home' size={30} color={tintColor} />
+            
+        }
+    },
+    Add: {
+        name:'AddPhoto',
+        screen: Feed,
+        navigationOptions: {
+            title:'Add Picture',
+            tabBarIcon: ({tintColor}) =>  
+                <Icon name='camera' size={30} color={tintColor} />
+            
+        }
+    },
+    Profile: {
+        name:'Profile',
+        screen: Feed,
+        navigationOptions: {
+            title:'Profile',
+            tabBarIcon: ({tintColor}) => 
+                <Icon name='user' size={30} color={tintColor} />
+            
+        }
+    }
+}
+
+const TabNavigatorConfig = {    
+    initialRouteName: 'Feed',
+    tabBarOptions: {
+            showLabel: false,
+    }
+}
+
+const navigator = createBottomTabNavigator(RouteConfigs,TabNavigatorConfig)
+const menuNavigator = createAppContainer(navigator)
+export default menuNavigator
