e7f71085cef2a57e1d0c984ca5072f4a853ce36a
Vitor <vitoraderaldo10@gmail.com>
2020-02-02 14:55:00 -0300

    Criada a function com deploy no firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,43 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true })
+const fs = require('fs')
+const uuid = require('uuid-v4')
+const { Storage } = require('@google-cloud/storage')
+const storage = new Storage({
+    projectId: 'instagram-d9d82',
+    keyFilename: 'keys.json'
+})
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+exports.uploadImage = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        try {
+            fs.writeFileSync('/tmp/imageToSave.jpg', 
+                request.body.image, 'base64')
+            
+            const bucket = storage.bucket('instagram-d9d82.appspot.com')
+            const id = uuid()
+            bucket.upload('/tmp/imageToSave.jpg', {
+                uploadType: 'media',
+                destination: `/posts/${id}.jpg`,
+                metadata: {
+                    metadata: {
+                        contentType: 'image/jpeg',
+                        firebaseStorageDownloadTokens: id
+                    }
+                }
+            }, (err, file) => {
+                if (err) {
+                    return response.status(500).json({ error: err })
+                } else {
+                    const fileName = encodeURIComponent(file.name)
+                    const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/'
+                        + bucket.name + '/o/' + fileName + '?alt=media&token=' + id
+                    return response.status(201).json({ imageUrl: imageUrl })
+                }
+            })
+        } catch (err) {
+            console.log(err)
+            return response.status(500).json({ error: err })
+        }
+    })
 });
