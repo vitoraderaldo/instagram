3c0cf3d791b2ac9643de4380706d094f80a0ad90
Vitor <vitoraderaldo10@gmail.com>
2020-02-04 22:37:30 -0300

    Utilizando Firebase para cadastrar o usuário

--- src/store/actions/User.js
+++ src/store/actions/User.js
@@ -1,4 +1,9 @@
 import {USER_LOGGED_IN,USER_LOGGED_OUT} from './ActionTypes'
+import axios from 'axios'
+import { Alert } from 'react-native'
+
+const authBaseURL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty'
+const API_KEY = 'AIzaSyDTT1Lz_4MSmgdHAKkw7YCO2sOsKFLw3r4'
 
 /*  Esta função é chamada de ActionCreator.
     Utilizada para escutar as mudanças da aplicação.
@@ -15,5 +20,27 @@ export const login = (user) => {
 export const logout = () => {
     return{
         type: USER_LOGGED_OUT        
+    }
+}
+
+export const createUser = user => {
+    return dispatch => {
+        axios.post(`${authBaseURL}/signupNewUser?key=${API_KEY}`,{
+            email: user.email,
+            password: user.password,
+            returnSecureToken: true
+        })
+        .catch(err => Alert.alert(err.toJSON()))
+        .then(res => {
+            if(res.data.localId){
+                axios.put(`/users/${res.data.localId}.json`,{
+                    name: user.name,
+                })
+                .catch(err => "Erro: "+console.log(err))
+                .then(res => {
+                    console.log("Usuário registrado com sucesso")
+                })
+            }
+        })
     }
 }
